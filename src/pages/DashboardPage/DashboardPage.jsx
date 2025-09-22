77d38f5bd30c23e70363071d76554abfdd7ca01d
SaajidMohammed <saajidmohammed6@gmail.com>
2025-09-22 16:23:26 +0530

    Update DashboardPage.jsx

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import './Dashboardpage.css';
+import './DashboardPage.css';
 
 const DashboardPage = () => {
   const location = useLocation();
